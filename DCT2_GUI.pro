5411ecf453bbb4ce56fc6dd98ac6d611aa79c118
Jan <emagiannu@yahoo.it>
2020-05-21 12:35:50 +0200

    Removed imageviewer

--- DCT2_GUI.pro
+++ DCT2_GUI.pro
@@ -16,12 +16,10 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    imageviewer.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
-    imageviewer.h \
     mainwindow.h
 
 FORMS += \
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include "imageviewer.h"
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    //ImageViewer* previewViewer = new ImageViewer();
-    //ui->previewLayout->addWidget(previewViewer);
     ui->setupUi(this);
 }
 
